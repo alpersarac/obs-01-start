270f3de35091160c689b44e7633e13e2bca4a657
alpersarac <alper.sarac42@gmail.com>
2023-07-08 16:30:05 +0300

    Error handling and complelation of the observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,11 +21,22 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(()=>{
         observer.next(count);
+        if(count===2){
+          observer.complete();
+        }
+        if(count>3){
+          observer.error(new Error('Count is greater than 3!!'));
+        }
         count++;
       },1000);
     });
     this.firstObsSubscription=customIntevalObservable.subscribe(data=>{
       console.log(data);
+    }, error=>{
+      console.log(error);
+      alert(error.message);
+    }, ()=>{
+      console.log('Completed');
     });
   }
   ngOnDestroy(): void {
