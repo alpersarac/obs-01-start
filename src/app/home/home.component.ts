5c78963a29632f8879a870923999a184aead957a
alpersarac <alper.sarac42@gmail.com>
2023-07-08 15:12:18 +0300

    Creating an observable with rxjs interval

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private firstObsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription=interval(1000).subscribe(count=>{
+      console.log(count);
+    })
+  }
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+
   }
 
 }
